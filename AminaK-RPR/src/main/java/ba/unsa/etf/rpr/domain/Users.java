91be803a724b9ce7149fb991f7b844fb49314abb
aminakurtovic <akurtovic4@etf.unsa.ba>
2022-12-19 15:51:45 +0100

    dodane klase i idable

---
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/domain/Users.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.domain;
+
+public class Users implements Idable{
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+}
