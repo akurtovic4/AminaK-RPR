94627fd70ee1b1a39bb8751dc6c2fea769db6ac2
aminakurtovic <akurtovic4@etf.unsa.ba>
2022-12-26 17:10:47 +0100

    home panel

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,4 +1,24 @@
 package ba.unsa.etf.rpr;
 
-public class AppFX {
+import ba.unsa.etf.rpr.controllers.HomeController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class AppFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
+        HomeController homeController = new HomeController();
+        fxmlLoader.setController(homeController);
+        Parent root = fxmlLoader.load();
+        stage.setTitle("Hotel tralala");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
 }
--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,4 +1,53 @@
 package ba.unsa.etf.rpr.controllers;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class HomeController {
+
+    public void showRegister(ActionEvent event){
+        try {
+            System.out.printf("registruj se");
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/register.fxml"));
+            fxmlLoader.setController(new RegisterController());
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Hotel Registracija");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.initStyle(StageStyle.UTILITY);
+            stage.setResizable(false);
+            stage.show();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void showExplore(ActionEvent event){
+        try {
+            System.out.printf("istrazi");
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/explore.fxml"));
+            fxmlLoader.setController(new ExploreController());
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Hotel Rooms");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.initStyle(StageStyle.UTILITY);
+            stage.setResizable(false);
+            stage.show();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
