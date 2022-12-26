c9c88dc7b1639e83aa94cc0c62144745682ede90
aminakurtovic <akurtovic4@etf.unsa.ba>
2022-12-26 17:32:09 +0100

    explore panel

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/ExploreController.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/ExploreController.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class ExploreController {
+
+    public void showRegister(ActionEvent event){
+        try {
+            System.out.printf("registruj se");
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/register.fxml"));
+            fxmlLoader.setController(new RegisterController());
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Hotel Registracija");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.initStyle(StageStyle.UTILITY);
+            stage.setResizable(false);
+            stage.show();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
