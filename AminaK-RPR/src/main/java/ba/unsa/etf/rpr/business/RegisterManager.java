24b06c55325a81047c94b9bb6cdfab36a2302206
aminakurtovic <akurtovic4@etf.unsa.ba>
2022-12-29 16:48:52 +0100

    validate username in bl

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/business/RegisterManager.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/business/RegisterManager.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr.business;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.exceptions.HotelException;
+
+import java.util.List;
 
 public class RegisterManager {
+
+    public void validateUsername(String name) throws HotelException{
+        if (name == null || name.length() > 25|| name.length() < 5){
+            throw new HotelException("Username must have between 5 and 25 letters");
+        }
+    }
+
+
+
 }
