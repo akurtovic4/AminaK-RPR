c83448504e4b771a6458405ec981505f9a96f823
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-04 12:02:06 +0100

    homeuser buttons made

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/HomeUserController.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/HomeUserController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class HomeUserController {
+    public Button btnEditProfile;
+    public Button btnLogOut;
+
+    public void showEditWindow(ActionEvent actionEvent) {
+    }
+
+    public void showHome(ActionEvent actionEvent) {
+    }
 }
