3b829dfb0e9b04569bbd04e9f8e0672ed770e157
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-04 19:30:32 +0100

    reserve actions added

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/MakeReservationController.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/MakeReservationController.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class MakeReservationController {
+    public DatePicker dpStart;
+    public ListView reservationList;
+    public Button btnReserve;
+    public Button btnGoBackUser;
+    public RadioButton radiobtnOne;
+    public ToggleGroup toggleGroup;
+    public RadioButton radiobtnTwo;
+    public RadioButton radiobtnThree;
+    public RadioButton radiobtnFour;
+    public DatePicker dpEnd;
+
+    public void btnMakeReservation(ActionEvent actionEvent) {
+    }
+
+    public void goBackToHome(ActionEvent actionEvent) {
+    }
 }
