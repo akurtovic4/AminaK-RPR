8c308978e15a5095185246a04bfa18ec9b041e85
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-04 12:52:27 +0100

    edit window connected

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/EditProfileConstroller.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/EditProfileConstroller.java
@@ -1,4 +1,34 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class EditProfileConstroller {
+    public Button btnSaveChanges;
+    public Button btnGoUserHome;
+
+    public void saveChanges(ActionEvent actionEvent) {
+    }
+
+    public void goUserHome(ActionEvent actionEvent) {
+        try{
+            Stage stage = (Stage) btnGoUserHome.getScene().getWindow();
+            stage.close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
+            fxmlLoader.setController(new HomeController());
+            Parent root = fxmlLoader.load();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
