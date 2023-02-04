3119267f7772ba8620cff8896a41453b323a9a23
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-04 12:13:17 +0100

    edit profile and reserve controllers and fxmls connected to other windows

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/HomeUserController.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/controllers/HomeUserController.java
@@ -1,15 +1,49 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class HomeUserController {
     public Button btnEditProfile;
     public Button btnLogOut;
 
     public void showEditWindow(ActionEvent actionEvent) {
+        try{
+            Stage stage = (Stage) btnEditProfile.getScene().getWindow();
+            stage.close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/edit.fxml"));
+            fxmlLoader.setController(new EditProfileConstroller());
+            Parent root = fxmlLoader.load();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     public void showHome(ActionEvent actionEvent) {
+
+        try{
+            Stage stage = (Stage) btnEditProfile.getScene().getWindow();
+            stage.close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
+            fxmlLoader.setController(new HomeController());
+            Parent root = fxmlLoader.load();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
