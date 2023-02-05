b209bc6a5756b26767d62c75beb9188a6f1e40be
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-05 20:31:16 +0100

    method for taking free rooms from databse

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/dao/RoomsDaoSQLImpl.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/dao/RoomsDaoSQLImpl.java
@@ -2,11 +2,15 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Room;
 import ba.unsa.etf.rpr.domain.RoomType;
+import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.HotelException;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -14,6 +18,12 @@ public class RoomsDaoSQLImpl extends AbstractDao<Room> implements RoomsDao{
     public RoomsDaoSQLImpl() {
         super("rooms");
     }
+
+
+
+
+
+
 
     @Override
     public Room row2object(ResultSet rs) throws HotelException {
@@ -40,4 +50,34 @@ public class RoomsDaoSQLImpl extends AbstractDao<Room> implements RoomsDao{
         item.put("price", object.getPrice());
         return item;
     }
+
+
+
+    public void reservedRooms(LocalDate startDate, LocalDate endDate) throws HotelException {
+
+        List<Room> sobe = new ArrayList<>();
+        String query = "SELECT * FROM rooms WHERE start_date >= startDate AND end_date <= endDate";
+/*
+        try{
+
+            PreparedStatement stmt = getConnection().prepareStatement(query);
+            stmt.setString(1, email);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+               Room result = row2object(rs);
+                rs.close();
+                sobe.add(result)
+              return sobe;
+            } else {
+                throw new HotelException("Room not found");
+            }
+        }
+        catch (HotelException e) {
+            throw new HotelException(e.getMessage(), e);
+        }
+    }*/
+
+    }
+
+
 }
