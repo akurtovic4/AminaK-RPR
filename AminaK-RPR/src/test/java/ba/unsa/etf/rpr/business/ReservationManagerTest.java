1ffe1fd98fa7a22c22a11dc009e1420215dc70e9
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-07 00:33:15 +0100

    test for time validation added

--- AminaK-RPR/src/test/java/ba/unsa/etf/rpr/business/ReservationManagerTest.java
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/business/ReservationManagerTest.java
@@ -2,12 +2,17 @@ package ba.unsa.etf.rpr.business;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationManagerTest {
 
     @Test
     void isTimeValid() {
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = startDate.plusDays(5);
+       assertTrue(startDate.isBefore(endDate));
     }
 
     @Test
