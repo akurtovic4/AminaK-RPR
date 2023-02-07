c717c71ca2381b611bc99a1758a1424e33201c07
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-07 01:04:57 +0100

    test folder for homecontroller added

---
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/controllers/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @Test
+    void showRegister() {
+    }
+
+    @Test
+    void showExplore() {
+    }
+
+    @Test
+    void showLogin() {
+    }
+}
