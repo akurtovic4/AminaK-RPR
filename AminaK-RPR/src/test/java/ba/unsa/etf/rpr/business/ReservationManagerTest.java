0dd2753de986898e0a57a3268fe38547cfe59f0b
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-08 16:12:23 +0100

    mokito testovi

--- AminaK-RPR/src/test/java/ba/unsa/etf/rpr/business/ReservationManagerTest.java
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/business/ReservationManagerTest.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.dao.ReservationsDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Reservation;
 import ba.unsa.etf.rpr.domain.Room;
 import ba.unsa.etf.rpr.domain.RoomType;
@@ -9,12 +10,43 @@ import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.HotelException;
 import org.junit.jupiter.api.Test;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.mockito.Mockito.when;
+import org.mockito.Mockito;
+
+import javax.xml.stream.events.Comment;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationManagerTest {
 
+    private ReservationManager reservationManager;
+    private Reservation reservation;
+    private  String comment = "tralala";
+    private ReservationsDaoSQLImpl reservationsDaoSQLMock;
+    private List<Reservation> reservations;
+
+    @BeforeEach
+    public void initializeObjectsWeNeed() {
+        reservationManager = Mockito.mock(ReservationManager.class);
+        reservation = new Reservation();
+       reservation.setComments(comment);
+        reservation.setId(50);
+
+        reservationsDaoSQLMock = Mockito.mock(ReservationsDaoSQLImpl.class);
+        reservations = new ArrayList<>();
+       // reservations.addAll(Arrays.asList(new Reservation(1,));
+    }
     @Test
     void isTimeValid() {
         LocalDate startDate = LocalDate.now();
@@ -24,9 +56,13 @@ class ReservationManagerTest {
 
     @Test
     void add() throws HotelException {
+//mokanje dao factory klase
+       // MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
+       // daoFactoryMockedStatic.when(DaoFactory::reservationsDao).thenReturn(reservationsDaoSQLMock);
         Reservation reservation = new Reservation();
         User user = new User("Amina", "Kurtovic", LocalDate.now(), 5, "aminko123&");
         Room room = new Room(1, 2.5F,1, RoomType.SINGLE_ROOM);
+
         reservation.setId(1);
         reservation.setStart(LocalDate.now());
         reservation.setEnd(LocalDate.now().plusDays(5));
