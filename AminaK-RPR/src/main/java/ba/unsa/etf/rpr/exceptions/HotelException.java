c70b2f89624b47ebd3c636cdd1da9dbb4f537842
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-08 14:00:20 +0100

    javadoc for exception

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/exceptions/HotelException.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/exceptions/HotelException.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * User defined exception, Hotel exception
+ */
 public class HotelException extends Exception {
     public HotelException(String msg, Exception reason){
         super(msg, reason);
