b818aaa34c8e31ce313bd9950f7b1e0ea5c15acc
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-08 12:49:57 +0100

    javadoc for RoomType enum

--- AminaK-RPR/src/main/java/ba/unsa/etf/rpr/domain/RoomType.java
+++ AminaK-RPR/src/main/java/ba/unsa/etf/rpr/domain/RoomType.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.domain;
 
+/**
+ * Enum of all possible room types
+ */
 public enum RoomType {
     SINGLE_ROOM, DOUBLE_ROOM, TRIPLE_ROOM, QUADRUPLE_ROOM, PRESIDENTIAL_SUITE
 }
