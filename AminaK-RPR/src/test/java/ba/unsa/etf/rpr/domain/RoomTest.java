2e1b1066fa9ab5eec5419e62a2d6bfa70867f50a
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-09 11:03:31 +0100

    more tests for Room domain finished

--- AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
@@ -1,28 +1,37 @@
 package ba.unsa.etf.rpr.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class RoomTest {
 
+    private Room room = new Room(1, 2.5F, 3, RoomType.SINGLE_ROOM);
     @Test
     void getPrice() {
+    assertEquals(2.5F, room.getPrice());
     }
 
-    @Test
-    void setPrice() {
-    }
 
     @Test
     void getFloor() {
+        assertEquals(3, room.getFloor());
     }
 
-    @Test
-    void setFloor() {
-    }
+
 
     @Test
     void testToString() {
+
+
+        String expected = "Room" +
+                "{id=" + room.getId() +
+                ", price=" + room.getPrice() +
+                ", floor=" + room.getFloor() +
+                ", type=" + room.getType() + "}"; // put the expected value here
+        assertEquals(expected, room.toString());
+
     }
 }
