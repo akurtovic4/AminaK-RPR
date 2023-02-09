a5fafd120e3799eec941391d2d9c24c1e829660b
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-09 13:52:37 +0100

    test for domain User

---
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/UserTest.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getBirth_date() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
