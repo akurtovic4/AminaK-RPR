2a1d63c103ddc619fafdcd2c3ba27accfe53a4dc
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-09 11:10:50 +0100

    more tests for Reservartion domain (getters)

--- AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/ReservationTest.java
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/ReservationTest.java
@@ -2,27 +2,45 @@ package ba.unsa.etf.rpr.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationTest {
 
-    @Test
-    void getStart() {
-    }
+    Reservation reservation = new Reservation();
+    User user = new User("Amina123", "Kurtovic", LocalDate.now(), 5, "aminko123&");
+    Room room = new Room(1, 2.5F,1, RoomType.SINGLE_ROOM);
+
+
 
     @Test
-    void setStart() {
+    void getStart() {
+        reservation.setId(1);
+        reservation.setStart(LocalDate.now());
+        reservation.setEnd(LocalDate.now().plusDays(5));
+        reservation.setUser(user);
+        reservation.setComments("rpr");
+        reservation.setRoom(room);
+        assertEquals(LocalDate.now(), reservation.getStart());
     }
+
+
 
     @Test
     void getEnd() {
+        reservation.setId(1);
+        reservation.setStart(LocalDate.now());
+        reservation.setEnd(LocalDate.now().plusDays(5));
+        reservation.setUser(user);
+        reservation.setComments("rpr");
+        reservation.setRoom(room);
+        assertEquals(LocalDate.now().plusDays(5), reservation.getEnd());
     }
 
-    @Test
-    void setEnd() {
-    }
 
-    @Test
+   /* @Test
     void testEquals() {
-    }
+        assertEquals();
+    }*/
 }
