ec0f86dda83fa926e92392c70a333364cd065d97
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-09 10:32:43 +0100

    more tests for domain classes

---
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/ReservationTest.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationTest {
+
+    @Test
+    void getStart() {
+    }
+
+    @Test
+    void setStart() {
+    }
+
+    @Test
+    void getEnd() {
+    }
+
+    @Test
+    void setEnd() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
---
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void getFloor() {
+    }
+
+    @Test
+    void setFloor() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
