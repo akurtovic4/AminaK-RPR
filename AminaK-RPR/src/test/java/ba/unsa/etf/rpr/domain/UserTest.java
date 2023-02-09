e2a21d9962ef4e90f6a7585c6cbec286bcf1bb64
aminakurtovic <akurtovic4@etf.unsa.ba>
2023-02-09 13:58:23 +0100

    test for domain User getter passwords

--- AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/UserTest.java
+++ AminaK-RPR/src/test/java/ba/unsa/etf/rpr/domain/UserTest.java
@@ -1,28 +1,49 @@
 package ba.unsa.etf.rpr.domain;
 
+import ba.unsa.etf.rpr.dao.UserDao;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+    private User user = new User();
+    @BeforeEach
+    public void setUp() {
+
+
+        user.setId(1);
+        user.setBirth_date(LocalDate.now());
+        user.setPassword("Amina123&");
+        user.setEmail("amina12345");
+        user.setName("Aminko");
+
+    }
 
     @Test
     void getName() {
+        assertEquals("Aminko", user.getName());
     }
 
     @Test
     void getEmail() {
+        assertEquals("amina12345", user.getEmail());
     }
 
     @Test
     void getBirth_date() {
+        assertEquals(LocalDate.now(), user.getBirth_date());
     }
 
     @Test
     void getPassword() {
+        assertEquals("Amina123&", user.getPassword());
     }
-
+/*
     @Test
     void testToString() {
-    }
+    }*/
 }
